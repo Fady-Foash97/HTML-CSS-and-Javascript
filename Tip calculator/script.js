c63816c454887bbf53e8cf295956d6141db057ec
Fady Ashraf <fadysamweal8@gmail.com>
2025-10-19 19:55:15 +0300

    Add files via upload

---
+++ Tip calculator/script.js
@@ -0,0 +1,13 @@
+const calculate = document.getElementById("calculate");
+const bill = document.getElementById("bill");
+const tip = document.getElementById("tip");
+const total = document.getElementById("total");
+
+function calculateTotal() {
+    const billValue = bill.value;
+    const tipValue = tip.value;
+    const totalValue = billValue * (1 + tipValue / 100);
+    total.innerText = totalValue;
+}
+
+calculate.addEventListener("click", calculateTotal);
